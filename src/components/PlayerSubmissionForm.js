9ae4ffad2404b9405e472cae5f406642f0c93887
Alice Boone <alicegbonel@gmail.com>
2021-01-06 17:04:39 -0800

    add the inputs and update onSubmitForm

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -5,7 +5,14 @@ import './PlayerSubmissionForm.css';
 
 const PlayerSubmissionForm = (props) => {
 
-  const [inputForm, setInputForm] = useState();
+  const [inputForm, setInputForm] = useState({
+    adj1: '',
+    noun1: '',
+    adv: '',
+    verb: '',
+    adj2: '',
+    noun2: '',
+  });
 
   const onFormChange = (e) => {
     setInputForm({
@@ -17,27 +24,76 @@ const PlayerSubmissionForm = (props) => {
     e.preventDefault();
 
     props.sendSubmission(inputForm);
+
+    setInputForm({
+      adj1: '',
+      noun1: '',
+      adv: '',
+      verb: '',
+      adj2: '',
+      noun2: '',
+    });
+
   }
 
   const [poemLines, setPoemsLines] = useState([]);
 
   const onSubmitForm(e, newPoem) {
-    setInputForm([...inputForm, newPoem]);
+    setPoemsLines([...inputForm, newPoem]);
   }
   return (
     <div className="PlayerSubmissionForm">
       <h3>Player Submission Form for Player #{ props.index }</h3>
 
       <form className="PlayerSubmissionForm__form" onSubmit={onSubmitForm} >
+
         <div className="PlayerSubmissionForm__poem-inputs">
 
-          {
-            // Put your form inputs here... We've put in one below as an example
-          }
+          <span>The</span>
           <input
-            placeholder="hm..."
-            type="text" />
+            name="adj1"
+            placeholder="adjective"
+            type="text"
+            onChange={onFormChange}
+            value={inputForm.adj1} />
 
+          <input	         
+            name="noun1"
+            placeholder="noun"
+            type="text" 
+            onChange={onFormChange}
+            value={inputForm.noun1} />
+
+          <input
+            name="adv"
+            placeholder="adverb"
+            type="text"
+            onChange={onFormChange}
+            value={inputForm.adv} />
+
+          <input
+            name="verb"
+            placeholder="verb"
+            type="text"
+            onChange={onFormChange} 
+            value={inputForm.verb}/>
+
+          <span>the</span>
+
+          <input
+            name="adj2"
+            placeholder="adjective"
+            type="text"
+            onChange={onFormChange}
+            value={inputForm.adj2} />
+          <input
+            name="noun2"
+            placeholder="noun"
+            type="text"
+            onChange={onFormChange}
+            value={inputForm.noun2} />
+
+          <span>.</span>
         </div>
 
         <div className="PlayerSubmissionForm__submit">
